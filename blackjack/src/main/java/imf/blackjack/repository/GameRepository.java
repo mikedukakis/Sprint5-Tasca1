4d085b7d1bb8c59192863c5a1d200a3c43478d48
Ignasi Moya <imoyaf@gmail.com>
2024-10-04 12:04:43 +0200

    Corrections to repos

--- blackjack/src/main/java/imf/blackjack/repository/GameRepository.java
+++ blackjack/src/main/java/imf/blackjack/repository/GameRepository.java
@@ -3,9 +3,9 @@ package imf.blackjack.repository;
 import imf.blackjack.entity.Game;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface GameRepository extends ReactiveMongoRepository<Game, String> {
-    Flux<Game> findAllByPlayerName(String playerName);
+    Mono<Game> findById(String id);;
 }
--- blackjack/src/main/java/imf/blackjack/repository/PlayerRepository.java
+++ blackjack/src/main/java/imf/blackjack/repository/PlayerRepository.java
@@ -1,11 +1,11 @@
 package imf.blackjack.repository;
 
 import imf.blackjack.entity.Player;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
 @Repository
-public interface PlayerRepository extends ReactiveCrudRepository<Player, Long> {
-    Mono<Player> findByName(String name);
+public interface PlayerRepository extends R2dbcRepository<Player, String> {
+    Mono<Player> findById(String id);
 }
