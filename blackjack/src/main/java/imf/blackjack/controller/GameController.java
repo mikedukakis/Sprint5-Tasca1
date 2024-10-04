40e8334eebbd5423a2000c52099ff06e316a5663
Ignasi Moya <imoyaf@gmail.com>
2024-10-04 12:54:16 +0200

    Develop controllers

--- blackjack/src/main/java/imf/blackjack/controller/GameController.java
+++ blackjack/src/main/java/imf/blackjack/controller/GameController.java
@@ -1,4 +1,39 @@
 package imf.blackjack.controller;
 
+import imf.blackjack.entity.Game;
+import imf.blackjack.service.GameService;
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@Data
+@RestController
+@RequestMapping("/game")
 public class GameController {
+    private final GameService gameService;
+
+    @PostMapping("/new")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Game> createNewGame(@RequestBody String playerName) {
+        return gameService.createNewGame(playerName);
+    }
+
+    @GetMapping("/{id}")
+    public Mono<Game> getGameDetails(@PathVariable String id) {
+        return gameService.getGameDetails(id);
+    }
+
+    @PostMapping("/{id}/play")
+    public Mono<Game> makeMove(@PathVariable String id, @RequestBody String moveType) {
+        return gameService.makeMove(id, moveType);
+    }
+
+    @DeleteMapping("/{id}/delete")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Mono<Void> deleteGame(@PathVariable String id) {
+        return gameService.deleteGame(id);
+    }
+
 }
--- blackjack/src/main/java/imf/blackjack/controller/PlayerController.java
+++ blackjack/src/main/java/imf/blackjack/controller/PlayerController.java
@@ -1,4 +1,28 @@
 package imf.blackjack.controller;
 
+import imf.blackjack.entity.Player;
+import imf.blackjack.service.PlayerService;
+
+import lombok.Data;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import javax.management.monitor.MonitorNotification;
+
+@Data
+@RestController
+@RequestMapping("/player")
 public class PlayerController {
+    private final PlayerService playerService;
+
+    @PutMapping("/{playerId}")
+    public Mono<Player> changePlayerName(@PathVariable String playerId, @RequestBody String newName) {
+        return playerService.changePlayerName(playerId, newName);
+    }
+
+    @GetMapping("/ranking")
+    public Flux<Player> getPlayerRanking() {
+        return playerService.getPlayerRanking();
+    }
 }
