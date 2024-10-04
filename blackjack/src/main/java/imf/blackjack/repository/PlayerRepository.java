8fee3cd1ca9a266239abe71ddef5f9c1ce833ea3
Ignasi Moya <imoyaf@gmail.com>
2024-10-04 12:55:38 +0200

    Create get player ranking functionality on all layers

--- blackjack/src/main/java/imf/blackjack/repository/PlayerRepository.java
+++ blackjack/src/main/java/imf/blackjack/repository/PlayerRepository.java
@@ -4,8 +4,11 @@ import imf.blackjack.entity.Player;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
 @Repository
 public interface PlayerRepository extends R2dbcRepository<Player, String> {
     Mono<Player> findById(String id);
+    @Query("SELECT * FROM players ORDER BY wins DESC")
+    Flux<Player> findPlayersOrderedByWins();
 }
--- blackjack/src/main/java/imf/blackjack/service/PlayerService.java
+++ blackjack/src/main/java/imf/blackjack/service/PlayerService.java
@@ -21,4 +21,8 @@ public class PlayerService {
                     return playerRepository.save(player);
                 });
     }
+
+    public Flux<Player> getPlayerRanking() {
+        return playerRepository.findPlayersOrderedByWins();
+    }
 }
