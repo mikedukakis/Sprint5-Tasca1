b13b4428b5368b0f888b5a1d3990f007bdb2449f
Ignasi Moya <imoyaf@gmail.com>
2024-10-04 11:08:44 +0200

    Correct annotation

--- blackjack/src/main/java/imf/blackjack/repository/PlayerRepository.java
+++ blackjack/src/main/java/imf/blackjack/repository/PlayerRepository.java
@@ -2,8 +2,10 @@ package imf.blackjack.repository;
 
 import imf.blackjack.entity.Player;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
+@Repository
 public interface PlayerRepository extends ReactiveCrudRepository<Player, Long> {
     Mono<Player> findByName(String name);
 }
