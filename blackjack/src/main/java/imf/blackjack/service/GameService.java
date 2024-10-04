d5f70316719bdb35d2319cf6a7cd23f22048261f
Ignasi Moya <imoyaf@gmail.com>
2024-10-04 12:28:01 +0200

    Develop services

--- blackjack/src/main/java/imf/blackjack/service/GameService.java
+++ blackjack/src/main/java/imf/blackjack/service/GameService.java
@@ -1,4 +1,43 @@
 package imf.blackjack.service;
 
+import imf.blackjack.entity.Game;
+import imf.blackjack.entity.Player;
+import imf.blackjack.exception.GameNotFoundException;
+import imf.blackjack.repository.GameRepository;
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Data
+@Service
 public class GameService {
+
+    private final GameRepository gameRepository;
+
+    public Mono<Game> createNewGame(String playerName) {
+        Player player = new Player(new ObjectId().toHexString(), playerName);
+        Game game = new Game(/new ObjectId().toHexString(), player);
+        return gameRepository.save(game);
+    }
+
+    public Mono<Game> getGameDetails(String gameId) {
+        return gameRepository.findById(gameId)
+                .switchIfEmpty(Mono.error(new GameNotFoundException("Game not found with ID: " + gameId)));
+    }
+
+    public Mono<Void> deleteGame(String gameId) {
+        return gameRepository.findById(gameId)
+                .switchIfEmpty(Mono.error(new GameNotFoundException("Game not found with ID: " + gameId)))
+                .flatMap(gameRepository::delete);
+    }
+
+    public Mono<Game> makeMove(String gameId, String moveType) {
+        return gameRepository.findById(gameId)
+                .switchIfEmpty(Mono.error(new GameNotFoundException("Game not found with ID: " + gameId)))
+                .flatMap(game -> {
+                    processMove(game, moveType);
+                    return gameRepository.save(game);
+                });
+    }
 }
--- blackjack/src/main/java/imf/blackjack/service/PlayerService.java
+++ blackjack/src/main/java/imf/blackjack/service/PlayerService.java
@@ -1,4 +1,24 @@
 package imf.blackjack.service;
 
+import imf.blackjack.entity.Player;
+import imf.blackjack.exception.PlayerNotFoundException;
+import imf.blackjack.repository.PlayerRepository;
+import lombok.Data;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Data
+@Service
 public class PlayerService {
+    private final PlayerRepository playerRepository;
+
+    public Mono<Player> changePlayerName(String playerId, String newName) {
+        return playerRepository.findById(playerId)
+                .switchIfEmpty(Mono.error(new PlayerNotFoundException("Player not found with Id " + playerId)))
+                .flatMap(player -> {
+                    player.setName(newName);
+                    return playerRepository.save(player);
+                });
+    }
 }
