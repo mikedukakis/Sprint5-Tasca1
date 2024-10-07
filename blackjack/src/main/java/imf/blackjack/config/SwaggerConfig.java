a34330a0b763cd0349ace1f90f778cd42997f4aa
Ignasi Moya <imoyaf@gmail.com>
2024-10-07 10:18:08 +0200

    Correct Swagger config file

--- blackjack/src/main/java/imf/blackjack/config/SwaggerConfig.java
+++ blackjack/src/main/java/imf/blackjack/config/SwaggerConfig.java
@@ -1,20 +1,25 @@
 package imf.blackjack.config;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("imf.blackjack"))
-                .paths(PathSelectors.any())
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Blackjack API").version("1.0"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("blackjack")
+                .pathsToMatch("/game/**", "/player/**", "/ranking")
                 .build();
     }
 }
