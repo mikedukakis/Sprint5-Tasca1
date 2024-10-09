e6740796646dd4fa6c61aa0e32193c30c4aabfab
Ignasi Moya <imoyaf@gmail.com>
2024-10-09 21:42:56 +0200

    final (hopefully) cleanup

--- blackjack/src/main/java/imf/blackjack/repository/GameRepository.java
+++ blackjack/src/main/java/imf/blackjack/repository/GameRepository.java
@@ -1,6 +1,5 @@
 package imf.blackjack.repository;
 
-import com.mongodb.lang.NonNull;
 import imf.blackjack.entity.Game;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
@@ -8,6 +7,5 @@ import reactor.core.publisher.Mono;
 
 @Repository
 public interface GameRepository extends ReactiveMongoRepository<Game, String> {
-    @Override
-    Mono<Game> findById(@NonNull String id);
+    Mono<Game> findById(String id);
 }
